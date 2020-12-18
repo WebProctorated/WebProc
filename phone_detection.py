90dbd8eb22323e5542c9004775715df46da21359
tanishq719 <47773364+tanishq719@users.noreply.github.com>
2020-12-18 21:40:48 +0530

    partially working code

--- phone_detection.py
+++ phone_detection.py
@@ -1,26 +1,44 @@
-import cv2 as cv
-import numpy as np
+import cv2
 
+thres = 0.45 # Threshold to detect object
 
-phone_cascade = cv.CascadeClassifier('Phone_Cascade.xml')
-camera = cv.VideoCapture(0, cv.CAP_DSHOW)
+cap = cv2.VideoCapture(0,cv2.CAP_DSHOW)
+cap.set(3,1280)
+cap.set(4,720)
+cap.set(10,70)
 
-i = 0
+classNames= ['cell phone']
+# classFile = 'coco.names'
+# with open(classFile,'rt') as f:
+#     classNames = f.read().rstrip('\n').split('\n')
+
+configPath = 'ssd_mobilenet_v3_large_coco_2020_01_14.pbtxt'
+weightsPath = 'frozen_inference_graph.pb'
+
+net = cv2.dnn_DetectionModel(weightsPath,configPath)
+net.setInputSize(320,320)
+net.setInputScale(1.0/ 127.5)
+net.setInputMean((127.5, 127.5, 127.5))
+net.setInputSwapRB(True)
 
 while True:
-    ret, frame = camera.read()
-    cv.imshow('frame', frame)
-    gray = cv.cvtColor(frame, cv.COLOR_BGR2GRAY)
-    # phones is basically the list of tuples containing rectangular coordinates of the phones detected
-    phones = phone_cascade.detectMultiScale(gray, 3, 9)
+    success,img = cap.read()
+    classIds, confs, bbox = net.detect(img,confThreshold=thres)
+    # print(classIds,confs,bbox)
 
-    if len(phones) > 0:
-        i += 1
-        print("yes {}".format(i))
+    if len(classIds) != 0:
+        for classId, confidence,box in zip(classIds.flatten(),confs.flatten(),bbox):
+            print(classId)
+            if classId == 77:
+                cv2.rectangle(img,box,color=(0,255,0),thickness=2)
+                cv2.putText(img,classNames[0].upper(),(box[0]+10,box[1]+30),
+                            cv2.FONT_HERSHEY_COMPLEX,1,(0,255,0),2)
+                cv2.putText(img,str(round(confidence*100,2)),(box[0]+200,box[1]+30),
+                            cv2.FONT_HERSHEY_COMPLEX,1,(0,255,0),2)
 
-    key = cv.waitKey(1)
-    if key == ord('q'):
+    cv2.imshow("Output",img)
+    if cv2.waitKey(1) == ord('q'):
         break
 
-camera.release()
-cv.destroyAllWindows()
+cap.release()
+cv2.destroyAllWindows()
