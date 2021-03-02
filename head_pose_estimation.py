d447473ca7d27f2b36960b8ea20cea54bb29d36c
Savi R Bhide <47332901+savi-bhide@users.noreply.github.com>
2021-03-02 17:26:36 +0530

    Add files via upload
    Files to be kept in the same folder while execution of head_pose_estimation.py .
    1. face_detector.py
    2. face_landmarks.py
    3. frozen_inference_graph.pb
    4. haarcascade_frontalface_default.xml
    5. ssd_mobilenet_v3_large_coco_2020_01_14.pbtxt
    6. coco.names

--- head_pose_estimation.py
+++ head_pose_estimation.py
@@ -1,11 +1,22 @@
-# -*- coding: utf-8 -*-
+"""
 
+
+Files to be kept in same folder while excution of this file.
+1. face_detector.py
+2. face_landmarks.py
+3. frozen_inference_graph.pb
+4. haarcascade_frontalface_default.xml
+5. ssd_mobilenet_v3_large_coco_2020_01_14.pbtxt
+6. coco.names
+
+
+"""
 
 import cv2
 import numpy as np
 import math
 from face_detector import get_face_detector, find_faces
-from face_landmarks import get_landmark_model, detect_marks
+from face_landmarks import get_landmark_model, detect_marks, draw_marks
 
 def get_2d_points(img, rotation_vector, translation_vector, camera_matrix, val):
     """Return the 3D points present as 2D for making annotation box"""
@@ -119,14 +130,45 @@ def head_pose_points(img, rotation_vector, translation_vector, camera_matrix):
     x = point_2d[2]
     
     return (x, y)
-    
+
+# Phone detection
+thres = 0.45 # Threshold to detect object
+classNames = ["cell phone"]
+configPath = 'ssd_mobilenet_v3_large_coco_2020_01_14.pbtxt'
+weightsPath = 'frozen_inference_graph.pb'
+
+L_phone = list()
+net = cv2.dnn_DetectionModel(weightsPath,configPath)
+net.setInputSize(320,320)
+net.setInputScale(1.0/ 127.5)
+net.setInputMean((127.5, 127.5, 127.5))
+net.setInputSwapRB(True)
+
+
+# Head orientation
+L_head_ori = list()   
+L_head_val = list() 
 face_model = get_face_detector()
 landmark_model = get_landmark_model()
+
+# Number of peoples 
+L_counts = list()
+fac = cv2.CascadeClassifier('D:/image+video/haarcascade_frontalface_default.xml')
+
+
+# Mouth orientation 
+L_mouth = list()
+outer_points = [[49, 59], [50, 58], [51, 57], [52, 56], [53, 55]]
+d_outer = [0]*5
+inner_points = [[61, 67], [62, 66], [63, 65]]
+d_inner = [0]*3
+
 cap = cv2.VideoCapture(0)
 ret, img = cap.read()
 size = img.shape
 font = cv2.FONT_HERSHEY_SIMPLEX 
-# 3D model points.
+
+# 3D model points. for head pose
 model_points = np.array([
                             (0.0, 0.0, 0.0),             # Nose tip
                             (0.0, -330.0, -65.0),        # Chin
@@ -144,14 +186,81 @@ camera_matrix = np.array(
                          [0, focal_length, center[1]],
                          [0, 0, 1]], dtype = "double"
                          )
+
+# Orientation of mouth points 
+while(True):
+    ret, img = cap.read()
+    rects = find_faces(img, face_model)
+    for rect in rects:
+        shape = detect_marks(img, landmark_model, rect)
+        draw_marks(img, shape)
+        cv2.putText(img, 'Press r to record Mouth distances', (30, 30), font,
+                    1, (0, 255, 255), 2)
+        cv2.imshow("Output", img)
+    if cv2.waitKey(1) & 0xFF == ord('r'):
+        for i in range(100):
+            for i, (q1, q2) in enumerate(outer_points):
+                d_outer[i] += shape[q2][1] - shape[q1][1]
+            for i, (q1, q2) in enumerate(inner_points):
+                d_inner[i] += shape[q2][1] - shape[q1][1]
+        break
+cv2.destroyAllWindows()
+d_outer[:] = [x / 100 for x in d_outer]
+d_inner[:] = [x / 100 for x in d_inner]
+
+
+# Main video streaming with Head and mouth orientation 
 while True:
     ret, img = cap.read()
-    if ret == True:
-        faces = find_faces(img, face_model)
-        for face in faces:
-            marks = detect_marks(img, landmark_model, face)
-            # mark_detector.draw_marks(img, marks, color=(0, 255, 0))
-            image_points = np.array([
+
+    # Head count module
+    if(ret):
+        gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+        faces = fac.detectMultiScale(gray, 1.1, 4)
+        L_counts.append(len(faces))
+    
+    # Phone detection module
+    classIds, confs, bbox = net.detect(img,confThreshold=thres)
+    # print(classIds,confs,bbox)
+    if len(classIds) != 0:
+        for classId, confidence,box in zip(classIds.flatten(),confs.flatten(),bbox):
+            
+            if classId == 77:
+                cv2.rectangle(img,box,color=(0,255,0),thickness=2)
+                cv2.putText(img, classNames[0].upper(), (box[0]+10, box[1]+30),
+                            cv2.FONT_HERSHEY_COMPLEX,1,(0,255,0),2)
+                cv2.putText(img,str(round(confidence*100,2)),(box[0]+200,box[1]+30),
+                            cv2.FONT_HERSHEY_COMPLEX,1,(0,255,0),2)
+                L_phone.append("Phone found")
+            else:
+                L_phone.append(" ")
+                
+                
+    # Head orientation both modules
+    faces = find_faces(img, face_model)
+    for face in faces:
+        marks = detect_marks(img, landmark_model, face)
+        
+        # Mouth opening part 
+        cnt_outer = 0
+        cnt_inner = 0
+        draw_marks(img, marks[48:])
+        for i, (q1, q2) in enumerate(outer_points):
+            if d_outer[i] + 3 < marks[q2][1] - marks[q1][1]:
+                cnt_outer += 1 
+        for i, (q1, q2) in enumerate(inner_points):
+            if d_inner[i] + 2 <  marks[q2][1] - marks[1][1]:
+                cnt_inner += 1
+        if cnt_outer > 3 and cnt_inner > 2:
+            print('Mouth open')
+            L_mouth.append("Mouth open")
+            cv2.putText(img, 'Mouth open', (30, 30), font,1, (0, 255, 255), 2)
+            
+        else:
+            L_mouth.append(" ")
+            
+        # Head orientation part        
+        image_points = np.array([
                                     marks[30],     # Nose tip
                                     marks[8],     # Chin
                                     marks[36],     # Left eye left corner
@@ -159,58 +268,78 @@ while True:
                                     marks[48],     # Left Mouth corner
                                     marks[54]      # Right mouth corner
                                 ], dtype="double")
-            dist_coeffs = np.zeros((4,1)) # Assuming no lens distortion
-            (success, rotation_vector, translation_vector) = cv2.solvePnP(model_points, image_points, camera_matrix, dist_coeffs, flags=cv2.SOLVEPNP_UPNP)
+        dist_coeffs = np.zeros((4,1)) # Assuming no lens distortion
+        (success, rotation_vector, translation_vector) = cv2.solvePnP(model_points, image_points, camera_matrix, dist_coeffs, flags=cv2.SOLVEPNP_UPNP)
             
             
             # Project a 3D point (0, 0, 1000.0) onto the image plane.
             # We use this to draw a line sticking out of the nose
             
-            (nose_end_point2D, jacobian) = cv2.projectPoints(np.array([(0.0, 0.0, 1000.0)]), rotation_vector, translation_vector, camera_matrix, dist_coeffs)
+        (nose_end_point2D, jacobian) = cv2.projectPoints(np.array([(0.0, 0.0, 1000.0)]), rotation_vector, translation_vector, camera_matrix, dist_coeffs)
             
-            for p in image_points:
-                cv2.circle(img, (int(p[0]), int(p[1])), 3, (0,0,255), -1)
+        for p in image_points:
+            cv2.circle(img, (int(p[0]), int(p[1])), 3, (0,0,255), -1)
             
-            
-            p1 = ( int(image_points[0][0]), int(image_points[0][1]))
-            p2 = ( int(nose_end_point2D[0][0][0]), int(nose_end_point2D[0][0][1]))
-            x1, x2 = head_pose_points(img, rotation_vector, translation_vector, camera_matrix)
+        p1 = ( int(image_points[0][0]), int(image_points[0][1]))
+        p2 = ( int(nose_end_point2D[0][0][0]), int(nose_end_point2D[0][0][1]))
+        x1, x2 = head_pose_points(img, rotation_vector, translation_vector, camera_matrix)
 
-            cv2.line(img, p1, p2, (0, 255, 255), 2)
-            cv2.line(img, tuple(x1), tuple(x2), (255, 255, 0), 2)
-            
-            try:
-                m = (p2[1] - p1[1])/(p2[0] - p1[0])
-                ang1 = int(math.degrees(math.atan(m)))
-            except:
-                ang1 = 90
+        cv2.line(img, p1, p2, (0, 255, 255), 2)
+        cv2.line(img, tuple(x1), tuple(x2), (255, 255, 0), 2)
+            # for (x, y) in marks:
+            #     cv2.circle(img, (x, y), 4, (255, 255, 0), -1)
+            # cv2.putText(img, str(p1), p1, font, 1, (0, 255, 255), 1)
+        try:
+            m = (p2[1] - p1[1])/(p2[0] - p1[0])
+            ang1 = int(math.degrees(math.atan(m)))
+        except:
+            ang1 = 90
                 
-            try:
-                m = (x2[1] - x1[1])/(x2[0] - x1[0])
-                ang2 = int(math.degrees(math.atan(-1/m)))
-            except:
-                ang2 = 90
+        try:
+               m = (x2[1] - x1[1])/(x2[0] - x1[0])
+               ang2 = int(math.degrees(math.atan(-1/m)))
+        except:
+            ang2 = 90
                 
-            if ang1 >= 48:
-                print('Head down')
-                cv2.putText(img, 'Head down', (30, 30), font, 2, (255, 255, 128), 3)
-            elif ang1 <= -48:
-                print('Head up')
-                cv2.putText(img, 'Head up', (30, 30), font, 2, (255, 255, 128), 3)
-             
-            if ang2 >= 48:
-                print('Head right')
-                cv2.putText(img, 'Head right', (90, 30), font, 2, (255, 255, 128), 3)
-            elif ang2 <= -48:
-                print('Head left')
-                cv2.putText(img, 'Head left', (90, 30), font, 2, (255, 255, 128), 3)
+                # print('div by zero error')
             
-            cv2.putText(img, str(ang1), tuple(p1), font, 2, (128, 255, 255), 3)
-            cv2.putText(img, str(ang2), tuple(x1), font, 2, (255, 255, 128), 3)
-        cv2.imshow('img', img)
-        if cv2.waitKey(1) & 0xFF == ord('q'):
-            break
-    else:
+        if ang1 >= 48:
+            print('Head down')
+            L_head_ori.append("Head down")
+            #cv2.putText(img, 'Head down', (30, 30), font, 2, (255, 255, 128), 3)
+        elif ang1 <= -48:
+            print('Head up')
+            L_head_ori.append("Head up")
+            #cv2.putText(img, 'Head up', (30, 30), font, 2, (255, 255, 128), 3)
+            
+        else: 
+            L_head_ori.append(" ")
+        L_head_val.append(ang1)
+        if ang2 >= 48:
+            print('Head right')
+            L_head_ori.append("Head right")
+                #cv2.putText(img, 'Head right', (90, 30), font, 2, (255, 255, 128), 3)
+        elif ang2 <= -48:
+            print('Head left')
+            L_head_ori.append("Head left")
+                
+                #cv2.putText(img, 'Head left', (90, 30), font, 2, (255, 255, 128), 3)
+        else: 
+            L_head_ori.append(" ")
+        L_head_val.append(ang2)
+            #cv2.putText(img, str(ang1), tuple(p1), font, 2, (128, 255, 255), 3)
+            #cv2.putText(img, str(ang2), tuple(x1), font, 2, (255, 255, 128), 3)
+    cv2.imshow('img', img)
+    if cv2.waitKey(1) & 0xFF == ord('q'):
         break
+
 cv2.destroyAllWindows()
 cap.release()
+
+# List will have data points for head orientation and mouth opening 
+
+print(L_head_ori)
+print(L_mouth)
+print(L_phone)
+print(L_counts)
+print(L_head_val)
